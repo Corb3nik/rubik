e269671385b04ed6fb04cff3a5cdca3bd895c99f
Corb3nik <ian@subnet192.com>
2017-03-07 17:27:11 -0500

    Adds spider module model

---
+++ app/models/spider.rb
@@ -0,0 +1,3 @@
+class Spider < ApplicationRecord
+  belongs_to :project
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170307220251) do
+ActiveRecord::Schema.define(version: 20170307222544) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,4 +22,13 @@ ActiveRecord::Schema.define(version: 20170307220251) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "spiders", force: :cascade do |t|
+    t.bigint "project_id"
+    t.string "url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["project_id"], name: "index_spiders_on_project_id"
+  end
+
+  add_foreign_key "spiders", "projects"
 end
