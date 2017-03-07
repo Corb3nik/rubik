f6c6f32bf6bf1a2c3abe5e57e05a6349643eb358
Corb3nik <ian@subnet192.com>
2017-03-07 17:49:13 -0500

    Adds modules controller and routes for spider and dirb modules

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
 
   get ':root', to: 'pages#index'
-  resources :projects
+  resources :projects do
+    get :spider, controller: :modules
+    get :dirb, controller: :modules
+  end
   root 'pages#index'
 
 end
