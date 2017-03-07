0a2efdd23e353712d046deefbd5e837f33143633
Corb3nik <ian@subnet192.com>
2017-03-07 13:18:24 -0500

    Adds template python module

---
+++ lib/template.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python2
+
+import argparse
+
+class Template:
+    'Template module'
+
+    def __init__(self, root, output=False):
+        self.root = root
+        self.loot = { }
+        self.output = output
+
+    def run(self):
+        self.loot['template'] = "TEMPLATE"
+
+        if self.output:
+            print self.loot
+
+        return self.loot
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='This is a template module')
+    parser.add_argument('root', metavar='root', type=str,
+                    help='URL of the web challenge')
+
+    args = parser.parse_args()
+    module = Template(args.root, True)
+    module.run()
+
