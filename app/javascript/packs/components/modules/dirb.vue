4f48e796f6a392bd3751a0ecf2a55df2f20529ec
Corb3nik <ian@subnet192.com>
2017-05-02 01:27:11 -0400

    Adds first version of dirb UX

--- app/javascript/packs/components/modules/dirb.vue
+++ app/javascript/packs/components/modules/dirb.vue
@@ -1,6 +1,61 @@
 <template>
   <div class="dirb">
-    We're in the dirb!
+    <div class="row">
+      <div class="col-md-12">
+        <div class="card-group">
+          <div class="card">
+            <div class="card-header">
+              HTML
+            </div>
+            <div class="card-block">
+              <a href=".">/index.php</a></br>
+              <a href=".">/debug.php</a></br>
+              <a href=".">/about_me.html</a></br>
+              <a href=".">/flag.php</a></br>
+            </div>
+          </div>
+          <div class="card">
+            <div class="card-header">
+              JS
+            </div>
+            <div class="card-block">
+              <a href=".">/main.js</a></br>
+              <a href=".">/bootstrap-min.js</a></br>
+            </div>
+          </div>
+          <div class="card">
+            <div class="card-header">
+              CSS
+            </div>
+            <div class="card-block">
+              <a href=".">/main.css</a></br>
+            </div>
+          </div>
+          <div class="card">
+            <div class="card-header">
+              Misc
+            </div>
+            <div class="card-block">
+              <a href=".">/robots.txt</a></br>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <br/>
+
+    <div class="row">
+      <div class="col-md-12">
+        <div class="card">
+          <div class="card-header">Actions</div>
+          <div class="card-block text-right">
+            <a href="#" class="btn btn-danger">Reset Results</a>
+            <a href="#" class="btn btn-primary">Run</a>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
