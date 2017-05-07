bf9191b313528f457cc94a32c9b8d23676f3f6c2
Simon Thiboutôt <simonthiboutot@hotmail.com>
2017-05-07 08:32:03 -0400

    Set generator config.

--- config/application.rb
+++ config/application.rb
@@ -11,5 +11,14 @@ module Rubik
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
+    config.generators do |g|
+      g.assets          false
+      g.stylesheets     false
+      g.javascripts     false
+      g.helper          false
+      g.test_framework  nil
+    end
+
   end
 end
