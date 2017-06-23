8206311b0ebc33fec30dd95efb06ed1abd1fb7c4
Simon Thiboutôt <simonthiboutot@hotmail.com>
2017-06-23 12:47:12 -0400

    Move projects fetching logic in _list.vue.

--- app/javascript/packs/components/projects/_list.vue
+++ app/javascript/packs/components/projects/_list.vue
@@ -1,29 +1,70 @@
 <template>
-  <b-list-group>
-    <b-list-group-item
-      v-for="project in projects"
-      :to="projectToLink(project)"
-      :key="project.id">
+  <div>
+    <loading v-if="has_status('fetching')" />
+    <errors v-else-if="has_status('failed')" :errors="errors"/>
+    <b-list-group>
+      <b-list-group-item
+        v-for="project in projects"
+        :to="projectToLink(project)"
+        :key="project.id">
         {{ project.name }}
-    </b-list-group-item>
-  </b-list-group>
+      </b-list-group-item>
+    </b-list-group>
+  </div>
 </template>
 
 <script>
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
+import * as api from '../../api/projects.js'
+import Errors from '../shared/errors.vue'
+import Loading from '../shared/loading.vue'
 
 export default {
-  name: 'projects-list',
-  props: {
-    projects: {
-      type: Array
+  components: {
+    Errors,
+    Loading
+  },
+  data () {
+    return {
+      projects: [],
+      errors: null,
+      status: 'idle' // idle, fetching, succeeded, failed
+    }
+  },
+  computed: {
+    has_status: function (status) {
+      return (status) => {
+        return this.status === status
+      }
     }
   },
   methods: {
     projectToLink: function (project) {
-      return "projects/" + project.id
+      return `projects/${project.id}`
+    },
+    fetchData () {
+      this.status = 'fetching'
+      api.fetch_projects()
+        .then(response => {
+          this.status = 'succeeded'
+          this.projects = response.data
+        })
+        .catch((error) => {
+          this.status = 'failed'
+          if (error.response) {
+            let contentType = error.response.headers['content-type']
+            if (contentType.indexOf('application/json') != -1) {
+              this.errors = error.response.data
+            }
+          }
+        })
     }
+  },
+  created () {
+    this.fetchData()
+  },
+  watch: {
+    // Call again the method if the route changes.
+    '$route': 'fetchData'
   }
 }
 </script>
--- app/javascript/packs/components/projects/index.vue
+++ app/javascript/packs/components/projects/index.vue
@@ -2,15 +2,13 @@
   <div class="row">
     <div class="col-md-4">
       <h1>Projects</h1>
-      <project-list :projects="projects"></project-list>
+      <project-list></project-list>
     </div>
     <ProjectForm></ProjectForm>
   </div>
 </template>
 
 <script>
-import VueRouter from 'vue-router'
-import * as api from '../../api/projects.js'
 import ProjectList from './_list.vue'
 import ProjectForm from './_form.vue'
 
@@ -18,26 +16,6 @@ export default {
   components: {
     ProjectList,
     ProjectForm
-  },
-  data () {
-    return {
-      projects: [],
-      name: '',
-      root: '',
-      has_errors: false,
-      errors: null,
-      loading: false
-    }
-  },
-  computed: {},
-  methods: {},
-  created () {
-    this.loading = true
-    api.fetch_projects()
-      .then(response => {
-        this.loading = false
-        this.projects = response.data
-      })
   }
 }
 </script>
