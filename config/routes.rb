7ad446465c420d10b9e5995c1e33be7351a6da1b
Corb3nik <ian@subnet192.com>
2017-07-02 17:30:21 -0400

    updates routes

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,11 @@ Rails.application.routes.draw do
     get 'dirb', controller: :dirb, action: :index
     post 'dirb/run', controller: :dirb, action: :run
     post 'dirb/reset', controller: :dirb, action: :reset
+
+    get 'wappalyzer', controller: :wappalyzer, action: :index
+    post 'wappalyzer/run', controller: :wappalyzer, action: :run
+    post 'wappalyzer/reset', controller: :wappalyzer, action: :reset
+
   end
 
   resources :modules, only: [:index]
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170506031135) do
+ActiveRecord::Schema.define(version: 20170702212302) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,9 +36,22 @@ ActiveRecord::Schema.define(version: 20170506031135) do
     t.string "url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "content_type"
     t.index ["project_id"], name: "index_spiders_on_project_id"
+  end
+
+  create_table "wappalyzers", force: :cascade do |t|
+    t.string "name"
+    t.string "website"
+    t.string "category"
+    t.integer "confidence"
+    t.bigint "project_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["project_id"], name: "index_wappalyzers_on_project_id"
   end
 
   add_foreign_key "dirbs", "projects"
   add_foreign_key "spiders", "projects"
+  add_foreign_key "wappalyzers", "projects"
 end
